0187f1e953c48f7dd931accf20da57864a37aee7
vanishta <vanishta.changea@gmail.com>
2020-07-26 00:23:41 +0200

    Test

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,6 +3,7 @@ const mongoose =require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URL,{
     useNewUrlParser:true,
-    useCreateIndex:true
+    useCreateIndex:true,
+    useUnifiedTopology: true
 });
 
